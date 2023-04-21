6b2a8c1753c79a7843cf2b56532a02a09c03ede4
TMInstaller <ajvldmlqjqclr@naver.com>
2023-04-21 19:51:45 +0900

    정적 컴포넌트 만들어보기 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 // /src/pages/index.tsx
 
 import Header from "./components/Header";
+import ShortWriting from "./components/ShortWriting";
 
 export default function Home() {
   return (
     <>
       <Header />
+      <ShortWriting />
     </>
   );
 }
